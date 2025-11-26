a270d43147f9c27f76c3bd25042b8ccfb854c54a
SogZiw <sogziw@foxmail.com>
2025-11-26 16:29:21 +0800

    add type

--- app/src/main/java/com/lib/notification/reminder/helper/AppLifecycleManager.kt
+++ app/src/main/java/com/lib/notification/reminder/helper/AppLifecycleManager.kt
@@ -6,14 +6,22 @@ import android.os.Bundle
 
 object AppLifecycleManager {
 
+    private val activityList = mutableSetOf<Activity>()
+
     private var foregroundCounts = 0
 
     fun isAppForeground() = foregroundCounts > 0
 
     fun init(context: Application) {
         context.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-            override fun onActivityCreated(activity: Activity, bundle: Bundle?) = Unit
-            override fun onActivityDestroyed(activity: Activity) = Unit
+            override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
+                synchronized(activityList) { activityList.add(activity) }
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+                synchronized(activityList) { activityList.remove(activity) }
+            }
+
             override fun onActivityPaused(activity: Activity) = Unit
             override fun onActivityResumed(activity: Activity) = Unit
             override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) = Unit
@@ -26,6 +34,19 @@ object AppLifecycleManager {
                 foregroundCounts--
             }
         })
+    }
+
+    private val adActivityTag = arrayOf("AdActivity", "AppLovin")
+
+    fun finishAdActivity() {
+        runCatching {
+            activityList.toMutableList().forEach { act ->
+                val actClzName = act::class.java.name
+                if (adActivityTag.any { actClzName.contains(it) }) {
+                    act.finish()
+                }
+            }
+        }
     }
 
 }
